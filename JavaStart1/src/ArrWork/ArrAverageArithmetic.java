68461dd5da769b645169b1f6212fe130eceaf1e7
Maxym Kalysh <503292@ukr.net>
2018-10-25 14:28:24 +0300

    arr average

--- JavaStart1/src/ArrWork/ArrAverageArithmetic.java
+++ JavaStart1/src/ArrWork/ArrAverageArithmetic.java
@@ -1,10 +1,25 @@
 package ArrWork;
 
+import java.util.Arrays;
+
+// вивести середнє арифметичне масива
+
 public class ArrAverageArithmetic {
 
 	public static void main(String[] args) {
-		
-		
+
+		int allNum = 0;
+		int average;
+
+		int[] arr = { 2, 5, 9, 1, 3, 4, 6, 346, 7, 10 };
+
+		for (int i = 0; i < arr.length; i++) {
+			allNum += arr[i];
+		}
+
+		average = allNum / arr.length;
+
+		System.out.println(average);
 
 	}
 
