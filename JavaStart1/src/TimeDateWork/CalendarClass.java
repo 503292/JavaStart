dcf44eb7ac85c32c1706baa0e6e98a3c13ad8dda
Maxym Kalysh <503292@ukr.net>
2018-10-29 13:00:59 +0200

    Calendar

--- JavaStart1/src/TimeDateWork/CalendarClass.java
+++ JavaStart1/src/TimeDateWork/CalendarClass.java
@@ -7,23 +7,22 @@ import java.util.Date;
 public class CalendarClass {
 
 	public static void main(String[] args) {
-		
+
 		Calendar c = Calendar.getInstance();
 		System.out.println("Year: " + c.get(Calendar.YEAR));
-		System.out.println("Month: " + c.get(Calendar.MONTH + 1)); // 0..11 
+		System.out.println("Month: " + (c.get(Calendar.MONTH) + 1)); // 0..11
 		System.out.println("Day: " + c.get(Calendar.DAY_OF_MONTH));
 
-		
 		c.add(Calendar.YEAR, 1);
 		c.add(Calendar.MONTH, 2);
 		c.add(Calendar.DAY_OF_MONTH, -2);
-	
+
 		Date dt = c.getTime();
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 		String res = sdf.format(dt);
 		System.out.println(res);
-		
+
 	}
 
 }
