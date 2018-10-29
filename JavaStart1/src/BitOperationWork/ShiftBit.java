0a36bf86254c9de452cbe1a8e0c91010dd82b0bd
Maxym Kalysh <503292@ukr.net>
2018-10-29 14:33:10 +0200

    Bit Shift

--- JavaStart1/src/BitOperationWork/ShiftBit.java
+++ JavaStart1/src/BitOperationWork/ShiftBit.java
@@ -3,8 +3,45 @@ package BitOperationWork;
 public class ShiftBit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 20;
+		int y = 45678;
 
+		System.out.println("x = " + Integer.toBinaryString(x));
+		
+		System.out.println("x = " + toBinaryString(x));
+		System.out.println("y = " + toBinaryString(y));
+		
+		x = x << 1;
+		System.out.println("x = " + toBinaryString(x));
+		x <<= 2;
+		System.out.println("x = " + toBinaryString(x));
+		
+		y >>= 1;
+		System.out.println("y = " + toBinaryString(y));
+				
+		//x * 2
+		int n = 2;
+		System.out.println("n = " + toBinaryString(n));
+		n *= 2;
+		System.out.println("n = " + toBinaryString(n));
+		n *= 4;
+		System.out.println("n = " + toBinaryString(n));
+		
+		
+		int m = 2;
+		System.out.println("m = " + toBinaryString(m) + ", " + m);
+		m <<= 1; // * 2
+		System.out.println("m = " + toBinaryString(m) + ", " + m);
+		m <<= 2; // * 4
+		System.out.println("m = " + toBinaryString(m) + ", " + m);
+		m >>= 1; // div 2
+		System.out.println("m = " + toBinaryString(m) + ", " + m);
+	}
+	
+	
+	//FUNCTION
+	static String toBinaryString(int x) {
+		return String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0');
 	}
 
 }
